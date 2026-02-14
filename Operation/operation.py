bea9037f1e928bf45bb428238aba3ed26b49ce85
Srikant Panda <srikantap571@gmail.com>
2026-02-15 02:17:12 +0530

    Initial commit: add basic CLI file manager commands

--- Operation/operation.py
+++ Operation/operation.py
@@ -1,11 +1,24 @@
 import os
+import shutil
 
-# Navigation
-# default_path= '/home/s-fedora/'
-# default_symbol = '~$' 
+
+def pwd():
+    return os.getcwd()
 
 def cd(filepath):
     os.chdir(filepath)
     return os.getcwd()
-def ls():
-    return os.listdir(os.getcwd())
+
+def ls(filepath):
+    return os.listdir(filepath)
+
+def create(filepath):
+    os.mkdir(filepath)
+
+def delete(filepath):
+    os.rmdir(filepath)
+
+def copyfile(src,dst):
+    shutil.copy2(src,dst)
+def copydir(src,dst):
+    shutil.copytree(src,dst)
--- main.py
+++ main.py
@@ -12,30 +12,129 @@ while True:
     if filepath[0] == 'cd':
         try:
             status=op.cd(filepath[1])
-            pathIndecator = status
-            continue
+            if status.startswith('/home/s-fedora'):
+                temp = status.split('/')
+                pathIndecator = f'~{temp[3]}'
+                continue
+            else:
+                pathIndecator = status
+                continue
         except IndexError:
-            pstatus=op.cd('/home/s-fedora/')
+            # print(filepath[1])
+            status=op.cd('/home/s-fedora/')
+            pathIndecator = '~'
+            continue
         except FileNotFoundError:
             if len(filepath) >2:
-                print(f'python: cd: too many aruments')    
+                print(f'python: cd: too many aruments') 
+                continue   
             elif filepath[1] == '~':
                 status=op.cd('/home/s-fedora/')
+                pathIndecator = '~'
+                continue
             else:
                 print(f'cd: {filepath[1]}: No such file or directory')
+        # except PermissionError:
+        #     print('python: this need root access.')
     elif filepath[0] == 'ls':
-        status = op.ls()
         try:
             if filepath[1] == '-a':
-                for i in status:
+                files = op.ls(filepath[2])
+                for i in files:
                     print(i)
-                continue
-        except:
-           
-            for i in status:
-                if not i.startswith('.'):
+            else:
+                files =op.ls(filepath[1])
+                for i in files:
+                    if not i.startswith('.'):
+                        print(i)
+
+        except IndexError:
+            files = op.ls(op.pwd()) 
+            for i in files:
+                if '-a' in filepath:
                     print(i)
+                else:
+                    if not i.startswith('.'):
+                        print(i)
+        except FileNotFoundError:
+            if 'ls' and '-a' in command:
+                print(f'python: {filepath[2]}:  no such file or direcory')
+            else:
+                print(f'python: {filepath[1]}:  no such file or direcory')                
+        except PermissionError:
+            print('python: this need root access.')
+    elif filepath[0] == 'pwd':
+        try:
+            print(op.pwd())
+            continue
+        except OSError:
+            print('Something is wrong in this filesystem')
+        except FileNotFoundError:
+            print(f'python: {filepath[1]}: no such directory ')
+        except PermissionError:
+            print('Need root permission for this. Run the program using sudo.')    
+    elif filepath[0] == 'mkdir':
+        try:
+            if not '-p' in filepath: 
+                for i in filepath[1:]:
+                    op.create(i)
+            else:
+                for i in filepath[2:]:
+                    op.os.makedirs(i)
+        except FileExistsError:
+            print(f'mkdir: cannot create directory {filepath[1]}: File exists')
+        except FileNotFoundError:
+                print(f'mkdir: cannot create directory {filepath[1]}: No such file or directory \n\t use -p to make recuresive directory.')
+        except PermissionError:
+            print('python: this need root access.')  
+        # except:
+        #     if '-p' in filepath:
+        #         op.os.makedirs(filepath[-1])        
+    elif filepath[0] == 'touch':
+        try:
+            for i in filepath[1:]:
+                with open(i,'x'):
+                    pass
+        except FileNotFoundError:
+            print(f'touch: cannot touch {filepath[1]}: No such file or directory')
+        except FileExistsError:
+            if '-f' in filepath:
+                for i in filepath[2:]:
+                    with open(i,'w') as f:
+                        f.write('')
                     
+            else:
+                print(f'File exists: {filepath[1]}\n \tto replace use -f')
+    elif filepath[0] == 'rm':
+        try:
+            if op.os.path.isfile(filepath[1]):
+                for i in filepath[1:]:
+                    op.os.remove(i)
+            # elif op.os.path.isdir(filepath[2]):
+            else:
+                if '-r' in filepath or '-rf' in filepath:
+                    for i in filepath[2:]:
+                        op.delete(i)    
+        except IndexError:
+                print(f'rm: cannot remove {filepath[1]}: Is a directory\n\t user -r to delete.')
+        except FileNotFoundError:
+            print(f'rm: cannot remove {filepath[1]}: No such file or directory')
+        except OSError:
+            if '-rf' in filepath:
+                for i in filepath[2:]:
+                    op.shutil.rmtree(i)
+            else:
+                print(f'rm: cannot remove {filepath[-1]}: Is a non empty directory\n\t use -rf to delete.')
     
+    elif filepath[0] == 'cp':
+        try:
+            if op.os.path.isfile(filepath[1]):
+                op.copyfile(filepath[-2],filepath[-1])
+            else:
+                op.copydir(filepath[-2],filepath[-1])
+        except FileNotFoundError:
+            print(f'cp: cannot stat {filepath[-2]}: No such file or directory')
+
+
     else:
         print(f'python: {command}: command not found....')
